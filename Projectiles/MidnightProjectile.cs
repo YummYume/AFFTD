6e283af7a6e8b57e0f0b594d21d9ca149d595689
YummYume <givsoul@gmail.com>
2020-10-14 17:44:12 +0200

    Changes to Midnight's Sword

---
+++ Projectiles/MidnightProjectile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria;
+using static Terraria.ModLoader.ModContent;
+
+namespace AFFTD.Projectiles
+{
+    public class MidnightProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.Name = "Midnight's Ranged Attack";
+            projectile.width = 20;
+            projectile.height = 20;
+            projectile.damage = 22;
+            projectile.knockBack = 0; //we give a bit more damage than the sword itself, but no knockback
+            projectile.penetrate = 30;
+            projectile.timeLeft = 10000;
+            projectile.light = 0.10f;
+            projectile.extraUpdates = 1;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.tileCollide = true;
+            projectile.ignoreWater = true;
+        }
+        public override void AI()
+        {
+            //projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
+        }
+    }
+}
